9664c724c034401bda2d45e299c7f42d4febb124
Scott Daniels <daniels@research.att.com>
2014-03-18 14:52:03 -0400

    Initial push to the repo.

---
+++ const.go
@@ -0,0 +1,14 @@
+/*
+	Mnemonic:	const.go
+	Absrract:	These are constants that are shared by all packages (gizmos, managers
+				and main). There are likely to be very few of them. 
+	Date:		18 March 2014
+	Author: 	E. Scott Daniels
+*/
+
+
+package tegu
+
+const (
+	SWFL_VISITED int = 0x01			// switch was visited during shortest path search
+)
