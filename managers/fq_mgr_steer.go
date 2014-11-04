fbc84f5b18238daf2fe1238982702c7d4adc0406
Scott Daniels <daniels@research.att.com>
2014-11-04 18:20:03 -0500

    Initial checkin of fq_manager_steer.go and res_mgr_steer.go.

---
+++ managers/fq_mgr_steer.go
@@ -0,0 +1,189 @@
+// vi: sw=4 ts=4:
+
+/*
+
+	Mnemonic:	fq_mgr_steering
+	Abstract:	flow/queue manager functions that are directly related to steering
+				(broken out of fq_mgr to make merging easier).
+
+	Date:		03 Nov 2014
+	Author:		E. Scott Daniels
+
+	Mods:		
+*/
+
+package managers
+
+import (
+	//"encoding/json"
+	"fmt"
+	//"os"
+	"strings"
+
+	//"forge.research.att.com/gopkgs/bleater"
+	//"forge.research.att.com/gopkgs/clike"
+	"forge.research.att.com/gopkgs/ipc"
+	//"forge.research.att.com/tegu/gizmos"
+)
+
+
+/*
+	Send flow-mod(s) to the agent for steering. 
+	The fq_req contains data that are neither match or action oriented (priority, expiry, etc) are or 
+	macht or action only (late binding mac value), and a set of match and action paramters that are
+	applied depending on where they are found. 
+	Data expected in the fq_req:
+		Nxt_mac - the mac address that is to be set on the action as dest
+		Expiry  - the timeout for the fmod(s)
+		Ip1/2	- The src/dest IP addresses for match (one must be supplied)
+		Meta	- The meta value to set/match (both optional)
+		Swid	- The switch DPID or host name (ovs) (used as -h option)
+		Swport	- The switch port to match (inbound)
+		Table	- Table number to put the flow mod into
+		Rsub    - A list (space separated) of table numbers to resub to in the order listed.
+		Lbmac	- Assumed to be the mac address associated with the switch port when
+					switch port is -128. This is passed on the -i option to the 
+					agent allowing the underlying interface to do late binding
+					of the port based on the mac address of the mbox.
+		Pri		- Fmod priority
+
+	TODO: this needs to be expanded to be generic and handle all possible match/action parms
+			not just the ones that are specific to steering.  It will probably need an on-all
+			flag in the main request struct rather than deducing it from parms. 
+*/
+func send_stfmod_agent( data *Fq_req, ip2mac map[string]*string, hlist *string ) {
+
+	if data.Pri <= 0 {
+		data.Pri = 100
+	}
+
+
+	table := ""
+	if data.Table > 0 {
+		table = fmt.Sprintf( "-T %d ", data.Table )
+	} 
+	/*
+	//===== right now no restriction/checking on some kind of source/dest
+	else {														// for table 0 we insist on having at least a src IP or port or a dest ip
+		if data.Match.Ip1 == nil && data.Match.Ip2 == nil {
+			if data.Match.Swport == -1 {
+				fq_sheep.Baa( 0, "ERR: cannot set steering fmod: both source and dest IP addresses nil and no inbound switch port set" )
+				return
+			}
+		}
+	}
+	*/
+
+	match_opts := "--match"					// build match options
+
+	if data.Match.Meta != nil {
+		if *data.Match.Meta != "" {
+			match_opts += " -m " + *data.Match.Meta		// allow caller to override if they know better
+		}
+	} 
+
+	on_all := data.Swid == nil 							// if no switch id, then we write to all
+
+	if data.Match.Swport >= 0  {						// valid port
+		match_opts += fmt.Sprintf( " -i %d", data.Match.Swport )
+	} else {
+		if data.Match.Swport == -128 {				// late binding port, we sub in the late binding MAC that was given
+			if data.Lbmac != nil {
+				match_opts += fmt.Sprintf( " -i %s", *data.Lbmac )
+			} else {
+				fq_sheep.Baa( 1, "ERR: cannot set steering fmod: late binding port supplied, but late binding MAC was nil" )
+			}
+		}
+	}
+
+	smac := data.Match.Smac								// smac wins if both smac and sip are given
+	if smac == nil {
+		if data.Match.Ip1 != nil {						// src supplied, match on src
+			smac = ip2mac[*data.Match.Ip1]
+			if smac == nil {
+				fq_sheep.Baa( 0, "ERR: cannot set steering fmod: src IP did not translate to MAC: %s", *data.Match.Ip1 )
+				return
+			}
+		}
+	}
+	if smac != nil {
+		match_opts += " -s " + *smac
+	}
+
+	dmac := data.Match.Dmac								// dmac wins if both dmac and sip are given
+	if dmac == nil {
+		if data.Match.Ip2 != nil {						// src supplied, match on src
+			dmac = ip2mac[*data.Match.Ip2]
+			if dmac == nil {
+				fq_sheep.Baa( 0, "ERR: cannot set steering fmod: dst IP did not translate to MAC: %s", *data.Match.Ip2 )
+				return
+			}
+		}
+	}
+	if dmac != nil {
+		match_opts += " -d " + *dmac
+	}
+
+	if data.Match.Tpsport >= 0 && data.Protocol != nil {						// we allow 0 as that means match all of this protocol
+        match_opts += fmt.Sprintf( " -p %s:%d", *data.Protocol, data.Match.Tpsport )
+    }
+
+    if data.Match.Tpdport >= 0 && data.Protocol != nil {
+        match_opts += fmt.Sprintf( " -P %s:%d", *data.Protocol, data.Match.Tpdport )
+    }
+
+	action_opts := ""
+
+	if data.Action.Dmac != nil {						
+		action_opts += " -d " + *data.Action.Dmac
+	}
+	if data.Action.Smac != nil {
+		action_opts += " -s " + *data.Action.Smac
+	}
+
+	if data.Nxt_mac != nil {
+		action_opts += " -d " + *data.Nxt_mac			// add next hop if supplied -- last mbox won't have a next hop, but needs to exist to skip p100 fmod
+	}
+
+	if data.Action.Meta != nil {						// CAUTION: ovs barfs on the command if write metadata isn't last
+		if *data.Action.Meta != "" {
+			action_opts += " -m " + *data.Action.Meta
+		}
+	}
+
+	if data.Action.Resub != nil { 						// action options order may be sensitive; ensure -R is last
+		toks := strings.Split( *data.Action.Resub, " " )
+		for i := range toks {
+			action_opts += " -R ," + toks[i]
+		}
+	}
+
+	output := "-N"			// TODO: allow output action to be passed in
+
+	//action_opts = fmt.Sprintf( "--action %s -R ,0 -N", action_opts )		// set up actions; may be order sensitive so -R and -N LAST 
+	action_opts = fmt.Sprintf( "--action %s %s", action_opts, output )		// set up actions
+
+	base_json := `{ "ctype": "action_list", "actions": [ { "atype": "flowmod", "fdata": [ `
+
+	if on_all {											// blast the fmod to all switches
+		hosts := strings.Split( *hlist, " " )
+		for i := range hosts {
+
+			json := base_json
+			json += fmt.Sprintf( `"-h %s %s -t %d -p %d %s %s add 0xe5d br-int"`, hosts[i], table, data.Expiry, data.Pri, match_opts, action_opts )
+			json += ` ] } ] }`
+			fq_sheep.Baa( 1, ">>> json: %s", json )
+			tmsg := ipc.Mk_chmsg( )						// must have one per since we dont wait for an ack
+			tmsg.Send_req( am_ch, nil, REQ_SENDSHORT, json, nil )		// send as a short request to one agent
+		}
+	} else {															// fmod goes only to the named switch
+		json := base_json
+		json += fmt.Sprintf( `"-h %s -t %d -p %d %s %s add 0xe5d br-int"`, *data.Swid, data.Expiry, data.Pri, match_opts, action_opts )
+		json += ` ] } ] }`
+		fq_sheep.Baa( 1, ">>> json: %s", json )
+
+		tmsg := ipc.Mk_chmsg( )
+		tmsg.Send_req( am_ch, nil, REQ_SENDSHORT, json, nil )		// send as a short request to one agent
+	}
+	
+}
---
+++ managers/res_mgr_steer.go
@@ -0,0 +1,209 @@
+// vi: sw=4 ts=4:
+
+/*
+
+	Mnemonic:	res_mgr_steer
+	Abstract:	reservation manager functions that are directly related to steering
+				(broken out of fq_mgr to make merging easier).
+
+	Date:		03 Nov 2014
+	Author:		E. Scott Daniels
+
+	Mods:		
+*/
+
+package managers
+
+import (
+	//"encoding/json"
+	//"fmt"
+	//"os"
+	"strings"
+	"time"
+
+	//"forge.research.att.com/gopkgs/bleater"
+	"forge.research.att.com/gopkgs/clike"
+	"forge.research.att.com/gopkgs/ipc"
+	"forge.research.att.com/tegu/gizmos"
+)
+
+
+/*
+	Generate flow-mod requests to the fq-manager for a given src,dest pair and list of 
+	middleboxes.  This assumes that the middlebox list has been reversed if necessary. 
+	Either source (ep1) or dest (ep2) may be nil which indicates a "to any" or "from any"
+	intention. 
+
+	DANGER:  We generate the flowmods in reverse order which _should_ generate the highest 
+			priority f-mods first. This is absolutely necessary to prevent packet loops
+			on the switches which can happen if a higher priority rule isn't in place 
+			that would cause the lower priority rule to be skipped over. 
+
+	TODO: add transport layer port support
+*/
+func steer_fmods( ep1 *string, ep2 *string, mblist []*gizmos.Mbox, expiry int64, rname *string, proto *string, forward bool ) {
+	var (
+		fq_data *Fq_req
+		fq_match *Fq_parms
+		fq_action *Fq_parms
+		mb	*gizmos.Mbox							// current middle box being worked with (must span various blocks)
+	)
+
+	if expiry < 5 {									// refuse if too short
+		return
+	}
+
+	mstr := "0x00/0x02"								// meta data match string; match if mask 0x02 is not set
+	nmb := len( mblist )
+	for i := 0; i < nmb; i++ {						// check value of each in list and bail if any are nil
+		if mblist[i] == nil {
+			rm_sheep.Baa( 1, "IER: steer_fmods: unexpected nil mb i=%d nmb=%d", i,  nmb )
+			return
+		}
+	}
+
+	for i :=  nmb -1; i >= 0;  i-- {					// backward direction ep2->ep1
+		resub := "90 0"									// we resubmit to table 10 to set our meta data and then resub to 0 to catch openstack rules
+
+		if i == nmb - 1 {								// for last mb we need a rule that causes steering to be skipped based on mb mac
+			mb = mblist[i]
+			fq_match = &Fq_parms{						// new structs for each since they sit in fq manages quwue
+				Swport:	-1,								// port 0 is valid, so we need something that is ignored if not set later
+				Meta:	&mstr,
+				Ip1:  	ep1,
+				Tpdport: -1,
+				Tpsport: -1,
+			}
+
+			fq_action = &Fq_parms{
+				Resub: &resub,							// resubmit to table 90 to set meta info, then to 0 to get tunnel matches
+			}
+
+			fq_data = &Fq_req {							// generate data with just what needs to be there
+				Pri:	300,
+				Id:		rname,
+				Expiry:	expiry,
+				Match:	fq_match,
+				Action:	fq_action,
+			}
+
+			if proto != nil {								// set the protocol match port dest in forward direction, src in reverse
+				toks := strings.Split( *proto, ":" );
+				fq_data.Protocol = &toks[0]
+				if forward {
+					fq_data.Match.Tpdport = clike.Atoi( toks[1] )
+				} else {
+					fq_data.Match.Tpsport = clike.Atoi( toks[1] )
+				}
+			}
+
+			if ep1 != nil {									// if source is a specific address, then we need only one 300 rule
+				fq_data.Match.Ip1 = nil												// there is no source to match at this point
+				fq_data.Match.Smac = nil
+				fq_data.Match.Ip2 = ep2
+				fq_data.Swid, fq_data.Match.Swport = mb.Get_sw_port( )	// specific switch and input port needed for this fmod
+				fq_data.Lbmac = mb.Get_mac()							// fqmgr will need the mac if the port is late binding
+			} else {													// if no specific src, 100 rule lives on each switch, so we must put a 300 on each too
+				fq_data.Swid = nil										// force to all switches
+				fq_data.Match.Smac = mb.Get_mac()						// src for 300 is the last mbox
+			}
+
+			jstr, _ := fq_data.To_json( ) 
+			rm_sheep.Baa( 1, "write final fmod: %s", jstr )
+
+			msg := ipc.Mk_chmsg()
+			msg.Send_req( fq_ch, nil, REQ_ST_RESERVE, fq_data, nil )			// final flow-mod from the last middlebox out
+		}
+
+		fq_match = &Fq_parms{
+			Swport:	-1,								// port 0 is valid, so we need something that is ignored if not set later
+			Meta:	&mstr,
+		}
+
+		fq_action = &Fq_parms{
+			Resub: &resub,							// resubmit to table 10 to set meta info, then to 0 to get tunnel matches
+		}
+
+		fq_data = &Fq_req {						// fq-mgr request data
+			Id:		rname,
+			Expiry:	expiry,
+			Match: 	fq_match,
+			Action: fq_action,
+			Protocol: proto,
+		}
+		fq_data.Match.Ip1 = ep1
+		fq_data.Match.Ip2 = ep2
+
+		if proto != nil {								// set the protocol match port dest in forward direction, src in reverse
+			toks := strings.Split( *proto, ":" );
+			fq_data.Protocol = &toks[0]
+			if forward {
+				fq_data.Match.Tpdport = clike.Atoi( toks[1] )
+			} else {
+				fq_data.Match.Tpsport = clike.Atoi( toks[1] )
+			}
+		}
+	
+		if i == 0 {									// push the ingress rule (possibly to all switches)
+			fq_data.Pri = 100
+
+			mb = mblist[i]
+			if ep1 != nil {
+				_, _, fq_data.Swid, _ = get_hostinfo( ep1 )		// if a specific src host supplied, get it's switch and we'll land only one flow-mod on it
+			} else {
+				fq_data.Swid = nil								// if ep1 is undefined (all), then we need a f-mod on all switches to handle ingress case
+			}
+			fq_data.Action.Tpsport = -1							// invalid port when writing to all too
+			fq_data.Nxt_mac = mb.Get_mac( )
+			jstr, _ := fq_data.To_json( ) 
+			rm_sheep.Baa( 2, "write ingress fmod: %s", jstr )
+
+			msg := ipc.Mk_chmsg()
+			msg.Send_req( fq_ch, nil, REQ_ST_RESERVE, fq_data, nil )			// no response right now -- eventually we want an asynch error
+		} else {																// push fmod on the switch that connects the previous mbox matching packets from it and directing to next mbox
+
+			mb = mblist[i-1] 													// pull previous middlebox which will be the source and define the swtich and port for this rule
+			fq_data.Swid, fq_data.Match.Swport = mb.Get_sw_port( )	 			// specific switch and input port needed for this fmod
+			fq_data.Lbmac = mb.Get_mac()										// fqmgr will need the mac if the port is late binding (-128)
+			fq_data.Match.Smac = nil											// we match based on input port and dest mac, so no need for this
+			fq_data.Match.Ip1 = nil												// and no need for the source ip which fqmanager happily translates to a mac
+
+			fq_data.Pri = 200						// priority for intermediate flow-mods
+			mb = mblist[i]	 						// now safe to get next middlebox in the list
+			fq_data.Nxt_mac = mb.Get_mac( )
+			jstr, _ := fq_data.To_json( ) 
+			rm_sheep.Baa( 2, "write intemed fmod: %s", jstr )
+
+			msg := ipc.Mk_chmsg()
+			msg.Send_req( fq_ch, nil, REQ_ST_RESERVE, fq_data, nil )			// flow mod for each intermediate link in backwards direction
+		}
+	}
+}
+
+
+/*
+	Push the fmod requests to fq-mgr for a steering resrvation. 
+*/
+func push_st_reservation( p *gizmos.Pledge, rname string, ch chan *ipc.Chmsg ) {
+
+	ep1, ep2, _, _, _, conclude, _, _ := p.Get_values( )		// hosts, ports and expiry are all we need
+	now := time.Now().Unix()
+
+	ep1 = name2ip( ep1 )										// we work only with IP addresses; sets to nil if "" (L*)
+	ep2 = name2ip( ep2 )
+
+	nmb := p.Get_mbox_count()
+	mblist := make( []*gizmos.Mbox, nmb ) 
+	for i := range mblist {
+		mblist[i] = p.Get_mbox( i )
+	}
+	steer_fmods( ep1, ep2, mblist, conclude - now, &rname, p.Get_proto(), true )			// set forward fmods
+
+	nmb--
+	for i := range mblist {											// build middlebox list in reverse
+		mblist[nmb-i] = p.Get_mbox( i )
+	}
+	steer_fmods( ep2, ep1, mblist, conclude - now, &rname, p.Get_proto(), false )			// set backward fmods
+
+	p.Set_pushed()
+}
