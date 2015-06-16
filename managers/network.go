6b5515c019fc8781a8a147a94da78f7ca209638f
Daniels, Scott <daniels@research.att.com>
2015-06-16 13:43:55 -0400

    Corrected second potential coredump cause in network.go.

--- managers/network.go
+++ managers/network.go
@@ -792,7 +792,7 @@ func (n *Network) host_info( name *string ) ( ip *string, mac *string, swid *str
 	mac = n.ip2mac[*ip]
 	if mac != nil {
 		if h, ok = n.hosts[*mac]; !ok {
-			err = fmt.Errorf( "cannot find host (representation) based on IP and MAC: %s %s", *ip, *mac )
+			err = fmt.Errorf( "cannot find host (representation) for host %s based on IP and MAC: %s %s", *host, *ip, *mac )
 			return
 		}
 	} else {
@@ -803,6 +803,9 @@ func (n *Network) host_info( name *string ) ( ip *string, mac *string, swid *str
 	sw, swport := h.Get_switch_port( 0 )			// we'll blindly assume it's not a split network 
 	if sw != nil {
 		swid = sw.Get_id() 
+	} else {
+		err = fmt.Errorf( "cannot generate switch/port for %s", *name )
+		return
 	}
 
 	return
