ef58b46553bce7d237b5cb70dac8e5a1a42dbaf1
Daniels, Scott <daniels@research.att.com>
2015-07-06 08:41:13 -0400

    Version bump to tegu.go

--- main/tegu.go
+++ main/tegu.go
@@ -111,7 +111,8 @@
 				09 Jun 2015 : Added oneway bandwidth reservations.
 				16 Jun 2015 : Extended oneway bandwitdh reservations to include queues.
 				25 Jun 2015 : Corrected bug with mirror expiration and xlation of proj/vm to mac.
-				-2 Jul 2015 : Corrected physical host fetch bug in osif.
+				02 Jul 2015 : Corrected physical host fetch bug in osif.
+				06 Jul 2015 : Version bump
 
 	Version number "logic":
 				3.0		- QoS-Lite version of Tegu
@@ -146,7 +147,7 @@ func usage( version string ) {
 
 func main() {
 	var (
-		version		string = "v3.1.4/17025"		// 3.1.x == steering branch version (.2 steering only, .3 steering+mirror+lite)
+		version		string = "v3.1.4/17065"		// 3.1.x == steering branch version (.2 steering only, .3 steering+mirror+lite)
 		cfg_file	*string  = nil
 		api_port	*string						// command line option vars must be pointers
 		verbose 	*bool
